0b1c23997dea92c73ad4d3c231c0cb78a0ecd97d
PJ Martinez <pem001@ucsd.edu>
2016-04-13 11:23:40 -0700

    First commit

---
+++ app/src/androidTest/java/tests/JUnit_test.java
@@ -0,0 +1,29 @@
+package tests;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
+
+import com.example.pjfmartinez.testapplication.MainActivity;
+import com.example.pjfmartinez.testapplication.R;
+
+/**
+ * Created by pjfmartinez on 4/13/16.
+ */
+public class JUnit_test extends ActivityInstrumentationTestCase2<MainActivity>{
+
+    MainActivity mainActivity;
+    public JUnit_test() {
+
+        super(MainActivity.class);
+    }
+
+    public void test_first() {
+        mainActivity = getActivity();
+
+        TextView textView = (TextView) mainActivity.findViewById(R.id.tv1);
+        String tester = textView.getText().toString();
+
+        assertEquals("Hello World!", tester);
+
+    }
+}
